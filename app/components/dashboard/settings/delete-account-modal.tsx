bc1015cc266506966b12a42a840701ac1f26f0ad
jenkx60 <jenkinsuwagbai18@gmail.com>
2026-01-27 13:06:08 +0100

    update

--- app/components/dashboard/settings/delete-account-modal.tsx
+++ app/components/dashboard/settings/delete-account-modal.tsx
@@ -168,7 +168,7 @@ const DeleteAccountModal: React.FC<DeleteAccountModalProps> = ({ isOpen, onClose
 
           <div className="p-6 h-screen">
              <CustomHeader onClose={onClose} />
-             <div className='overflow-y-auto max-h-[85dvh]'>
+             <div className='overflow-y-auto min-h-[85dvh]'>
               <DeleteAccountForm onClose={onClose} handleDelete={handleDelete} />
              </div>
           </div>
